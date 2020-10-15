16ece54477062bbb54454b21ca86b15bd18483ee
Olga Stasyuk <ostasyuk@users.noreply.github.com>
2020-10-15 16:35:50 +0300

    Create outputs.tf

---
+++ terraform-aws-github/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = concat(aws_vpc.this.*.id, [""])[0]
+}
+
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = concat(aws_vpc.this.*.arn, [""])[0]
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = concat(aws_vpc.this.*.cidr_block, [""])[0]
+}
+
+output "default_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = concat(aws_vpc.this.*.default_security_group_id, [""])[0]
+}
